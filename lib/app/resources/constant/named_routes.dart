59176d2d463baa4c2b5608c112244fbe2632b43d
nadiaperestp83-eng <nadiaperestp83@gmail.com>
2026-07-09 19:38:47 -0300

    Update named_routes.dart

--- lib/app/resources/constant/named_routes.dart
+++ lib/app/resources/constant/named_routes.dart
@@ -3,6 +3,7 @@ class NamedRoutes {
   static const profileScreen = '/ticket-screen';
   static const friendsScreen = '/friends-screen';
   static const chatScreen = '/chat-screen';
+  static const chatConversationScreen = '/chat-conversation';
   static const loginScreen = '/login';
   static const signUpScreen = '/signup';
   static const forgotPasswordScreen = '/forgot-password';
