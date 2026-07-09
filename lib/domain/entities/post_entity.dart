ee888cf729842ef9cba4a66083421d0a33aad69c
nadiaperestp83-eng <nadiaperestp83@gmail.com>
2026-07-09 18:48:56 -0300

    Update post_entity.dart

--- lib/domain/entities/post_entity.dart
+++ lib/domain/entities/post_entity.dart
@@ -1,10 +1,16 @@
+import 'package:social_media_app/domain/entities/link_preview_entity.dart';
+import 'package:social_media_app/domain/entities/post_mention_entity.dart';
+
 class PostEntity {
   final String id;
+  final String authorId;
   final String authorName;
   final String authorAvatarUrl;
   final String? imageUrl;
   final String caption;
   final List<String> hashtags;
+  final List<PostMentionEntity> mentions;
+  final LinkPreviewEntity? linkPreview;
   final int likesCount;
   final int commentsCount;
   final int sharesCount;
@@ -13,11 +19,14 @@ class PostEntity {
 
   const PostEntity({
     required this.id,
+    required this.authorId,
     required this.authorName,
     required this.authorAvatarUrl,
     this.imageUrl,
     required this.caption,
     this.hashtags = const [],
+    this.mentions = const [],
+    this.linkPreview,
     this.likesCount = 0,
     this.commentsCount = 0,
     this.sharesCount = 0,
@@ -31,11 +40,14 @@ class PostEntity {
   }) {
     return PostEntity(
       id: id,
+      authorId: authorId,
       authorName: authorName,
       authorAvatarUrl: authorAvatarUrl,
       imageUrl: imageUrl,
       caption: caption,
       hashtags: hashtags,
+      mentions: mentions,
+      linkPreview: linkPreview,
       likesCount: likesCount ?? this.likesCount,
       commentsCount: commentsCount,
       sharesCount: sharesCount,
