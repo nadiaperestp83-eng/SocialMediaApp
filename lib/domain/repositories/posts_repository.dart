d8ad8181861b869f9a9008d7f904eea288290b1d
nadiaperestp83-eng <nadiaperestp83@gmail.com>
2026-07-09 12:00:52 -0300

    Update posts_repository.dart

--- lib/domain/repositories/posts_repository.dart
+++ lib/domain/repositories/posts_repository.dart
@@ -1,6 +1,12 @@
+import 'package:social_media_app/domain/entities/comment_entity.dart';
 import 'package:social_media_app/domain/entities/post_entity.dart';
 
 abstract class PostsRepository {
   Future<List<PostEntity>> fetchFeed();
   Future<void> toggleLike({required String postId, required bool like});
+
+  /// Stream ao vivo dos comentários de um post (Supabase Realtime).
+  Stream<List<CommentEntity>> watchComments(String postId);
+
+  Future<void> addComment({required String postId, required String content});
 }
