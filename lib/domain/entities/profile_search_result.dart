86b0eb8b2703a89b047285a2d3220ca9ce955294
nadiaperestp83-eng <nadiaperestp83@gmail.com>
2026-07-09 19:47:51 -0300

    Create profile_search_result.dart

---
+++ lib/domain/entities/profile_search_result.dart
@@ -0,0 +1,13 @@
+class ProfileSearchResult {
+  final String id;
+  final String name;
+  final String username;
+  final String avatarUrl;
+
+  const ProfileSearchResult({
+    required this.id,
+    required this.name,
+    required this.username,
+    required this.avatarUrl,
+  });
+}
