0e9bd3f7897650845723c7fc869cdaaecc2bb7d7
nadiaperestp83-eng <nadiaperestp83@gmail.com>
2026-07-10 07:38:48 -0300

    Update friends_repository.dart

--- lib/domain/repositories/friends_repository.dart
+++ lib/domain/repositories/friends_repository.dart
@@ -1,7 +1,10 @@
+import 'package:social_media_app/domain/entities/friend_entity.dart';
 import 'package:social_media_app/domain/entities/friendship_entity.dart';
 
 abstract class FriendsRepository {
   Future<FriendshipStatus> getStatus(String userId);
   Future<void> sendFriendRequest(String userId);
   Future<void> cancelRequest(String userId);
+  Future<void> acceptRequest(String userId);
+  Future<List<FriendEntity>> fetchFriends();
 }
