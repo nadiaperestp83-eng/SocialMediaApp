fd68fed5b11522fac85a6dc4d5b56213c8ebd936
nadiaperestp83-eng <nadiaperestp83@gmail.com>
2026-07-10 07:38:03 -0300

    Create friend_entity.dart

---
+++ lib/domain/entities/friend_entity.dart
@@ -0,0 +1,13 @@
+class FriendEntity {
+  final String id;
+  final String name;
+  final String username;
+  final String avatarUrl;
+
+  const FriendEntity({
+    required this.id,
+    required this.name,
+    required this.username,
+    required this.avatarUrl,
+  });
+}
